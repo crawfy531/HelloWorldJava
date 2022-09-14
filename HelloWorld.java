78b11600e8d1ed491e4d0d82d17fb573b9770f9c
crawfy531 <cra21014@byui.edu>
2022-09-14 13:34:37 -0600

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+//this is a comment
+
+//the first public class (usually called main) needs to match the name of the file
+
+public class HelloWorld {
+
+    //this is the main method that does not take in or take out variables to other methods
+    public static void main(String[] args) {
+        //this outputs in the system to print on the commandline "Hello World"
+        //must be "" and not '' in the parenthises
+        System.out.println("Hello World");
+    }
+}
